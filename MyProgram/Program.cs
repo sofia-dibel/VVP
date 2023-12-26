143ed073147dce985c3094b54a8555ce0347e553
sofia-dibel <105718658+sofia-dibel@users.noreply.github.com>
2023-12-26 13:52:10 +0300

    Факториал итерационно и рекурсивно

---
+++ MyProgram/Program.cs
@@ -0,0 +1,33 @@
+﻿// 13. Функция расчёта факториала итерационным и рекурсивным методами.
+
+
+// итерационный
+
+static long FactIter(long n)
+{
+    var y = (long)1;
+    for(var i = 2; i <= n; i++)
+    {
+        y *= i;
+    }
+    return y;
+}
+
+Console.WriteLine(FactIter(5));
+
+
+// рекурсивный
+
+static long FactRecursion(long n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return n * FactRecursion(n - 1);
+}
+
+Console.WriteLine(FactRecursion(5));
+
+
+
